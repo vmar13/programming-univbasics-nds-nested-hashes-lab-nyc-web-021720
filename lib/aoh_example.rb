48c6510f646cbcd113efebc507f6d103d7b6da1a
Vanessa Martinez <vmar76@gmail.com>
2020-02-13 01:21:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  array = [
+  array.sort = [
     DON_G,
     JOELLE_VD,
     PAT_M,
