c60f1dd310c7cf4feb726b8582a3ecec0fb46a23
Vanessa Martinez <vmar76@gmail.com>
2020-02-13 01:40:54 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,6 +20,11 @@ end
 
 def literal_aoh
   array = [
+    {name: "Don Gately", occupation: "Live-in Staff"},
+    {name: "Joelle van Dyne", occupation: "Radio Personality"},
+    {name: "Pat Monteseian", occupation: "Staff"},
+    {name: "Kate Gompert", occupation: "None"},
+    {name: "Bruce Green", occupation: "Fan of Mildred"}
     ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
